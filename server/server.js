4b3d1f48243466125d1fe90d1f1beb801837f2cc
Justin Cathey <95rocketman@gmail.com>
2018-03-14 09:41:59 +0300

    added support for socket.io with connect and disconnect handlers

--- server/server.js
+++ server/server.js
@@ -1,13 +1,27 @@
 const path = require('path');
 const publicPath = path.join(__dirname,'../public');
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
+
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+io.on('connection',(socket)=>{
+  console.log("new user connected to app");
+  socket.on('disconnect',()=>{
+    console.log('disconnected from client')
+  });
+});
 
-app.listen(port,()=>{
+
+
+server.listen(port,()=>{
   console.log(`started on port ${port}`);
 });
 module.exports = {app};
